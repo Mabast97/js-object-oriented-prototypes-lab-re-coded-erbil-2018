8d4137ebfe30fa2417097021158ab10203897479
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-31 20:38:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ BoardMember.prototype.veto = function (){ return ("No, I must disagree"); }
 BoardMember.prototype.approve = function(){ return ("You can do that!"); }
 BoardMember.prototype.doCharity = function(){ return ("I like to help people."); }
 BoardMember.prototype.releasePressStatement = function(){ return ("You will see great things from Scuber."); }
-BoardMember.prototype.sayHi = function(){ return ("You can do that!"); }
+BoardMember.prototype.sayHi = function(){ return (`Hi, my name is ${name}, and i was trained in ${training}.`); }
 
 
 
