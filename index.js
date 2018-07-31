ac1f67a202a5e8660463cedb6d6e78de56c79d97
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-31 20:41:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ function BoardMember(name, homeState, training )
   this.name = name;
   this.training = training;
   this.homeState = homeState;
-  
 }
+
 BoardMember.prototype.veto = function (){ return ("No, I must disagree"); }
 BoardMember.prototype.approve = function(){ return ("You can do that!"); }
 BoardMember.prototype.doCharity = function(){ return ("I like to help people."); }
 BoardMember.prototype.releasePressStatement = function(){ return ("You will see great things from Scuber."); }
-BoardMember.prototype.sayHi = function(){ return (`Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`); }
 
 
 
